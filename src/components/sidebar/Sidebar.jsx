c328f81debca9611ea59147faffe86a007213e6c
Ozan Gürer <ozangurer12@gmail.com>
2022-02-25 20:57:47 +0300

    Sidebar

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,159 @@
+import "./sidebar.css";
+import {
+  RssFeed,
+  Message,
+  PlayCircleFilledOutlined,
+  Group,
+  Bookmark,
+  HelpOutline,
+  WorkOutline,
+  Event,
+  School,
+} from "@material-ui/icons";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="siderbarListItem">
+            <RssFeed className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="siderbarListItem">
+            <Message className="sidebarIcon" />
+            <span className="sidebarListItemText">Chats</span>
+          </li>
+          <li className="siderbarListItem">
+            <PlayCircleFilledOutlined className="sidebarIcon" />
+            <span className="sidebarListItemText">Videos</span>
+          </li>
+          <li className="siderbarListItem">
+            <Group className="sidebarIcon" />
+            <span className="sidebarListItemText">Groups</span>
+          </li>
+          <li className="siderbarListItem">
+            <Bookmark className="sidebarIcon" />
+            <span className="sidebarListItemText">Bookmarks</span>
+          </li>
+          <li className="siderbarListItem">
+            <HelpOutline className="sidebarIcon" />
+            <span className="sidebarListItemText">Questions</span>
+          </li>
+          <li className="siderbarListItem">
+            <WorkOutline className="sidebarIcon" />
+            <span className="sidebarListItemText">Jobs</span>
+          </li>
+          <li className="siderbarListItem">
+            <Event className="sidebarIcon" />
+            <span className="sidebarListItemText">Events</span>
+          </li>
+          <li className="siderbarListItem">
+            <School className="sidebarIcon" />
+            <span className="sidebarListItemText">Courses</span>
+          </li>
+        </ul>
+        <button className="sidebarBtn">Show More</button>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/user/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Miller</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/user/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Miller</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/user/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Miller</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/user/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Miller</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/user/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Miller</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/user/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Miller</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/user/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Miller</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/user/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Miller</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/user/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Miller</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/user/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Miller</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/user/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Miller</span>
+          </li>
+          <li className="sidebarFriend">
+            <img
+              className="sidebarFriendImg"
+              src="/assets/user/2.jpeg"
+              alt=""
+            />
+            <span className="sidebarFriendName">Jane Miller</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
