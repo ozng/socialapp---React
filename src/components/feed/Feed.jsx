019ea60ec78038ef36c42d7aa584b84bc44d72ca
Ozan Gürer <ozangurer12@gmail.com>
2022-02-28 15:35:53 +0300

    Feed

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,5 +1,21 @@
 import "./feed.css";
+import Share from "../share/Share";
+import Post from "../post/Post";
 
 export default function Feed() {
-  return <div className="feed">Feed</div>;
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
 }
