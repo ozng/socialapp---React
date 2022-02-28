5c8f1d7637910f778aa82022321d190cf64f65ce
Ozan Gürer <ozangurer12@gmail.com>
2022-02-28 16:19:58 +0300

    rightbar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,31 @@
 import "./rightbar.css";
 
 export default function Rightbar() {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightBarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="/assets/gift.png" />
+          <span className="birthdayText">
+            <b>Scarlett Johansson</b> and <b>3 other</b> friends.
+          </span>
+        </div>
+        <img src="/assets/ad.jpeg" alt="" className="rightBarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/user/3.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Gemma Carter</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
